9232f089ce7a354332c4795cd14ffe3d494557c7
natade-jp <natade3@gmail.com>
2019-09-01 23:15:07 +0900

    up

--- main/99-create-dic.js
+++ main/99-create-dic.js
@@ -7,11 +7,49 @@ const MeCab = require("mecab-async");
 const mecab = new MeCab();
 MeCab.command = env["MECAB_RUN"];
 
-// 非同期処理を同期処理へ置き換える
+/**
+ * MeCab による解析データ
+ * @typedef {Object} Morphs
+ * @property {string} kanji ください
+ * @property {string} lexical 動詞
+ * @property {string} compound 非自立
+ * @property {string} compound2 *
+ * @property {string} compound3 *
+ * @property {string} conjugation 五段・ラ行特殊
+ * @property {string} inflection 命令ｉ
+ * @property {string} original くださる
+ * @property {string} reading クダサイ
+ * @property {string} pronunciation クダサイ
+ * @property {string} kanji ください
+ */
+
+/**
+ * parseFormat の結果から読みを取得する
+ * @param {Morphs[]} morphs 
+ * @param {{hiragana : string, romaji : string}}
+ */
+const getYomiData = function(morphs) {
+	let reading = ""
+	for(let i = 0; i < morphs.length; i++) {
+		const morph = morphs[i];
+		reading += morph.reading
+	}
+	return {
+		hiragana : MojiJS.toHiragana(reading),
+		romaji : MojiJS.toRomajiFromKatakana(reading).toUpperCase()
+	};
+}
+
+/**
+ * Mecabを使用してテキストを解析する
+ * 非同期処理を同期処理へ置き換える
+ * @param {string} text 
+ * @returns {*} 読み
+ */
 const parseFormat = (text) => {
 	return new Promise((resolve, reject) => {
 		MeCab.parseFormat(text, (err, morphs) => {
-			resolve(morphs);
+			resolve(getYomiData(morphs));
 		});
 	});
 }
@@ -21,36 +59,72 @@ const main = async() => {
 	const kisoku = File.loadTextFile("./dict/main/memo.kisoku");
 	const kisoku_lines = kisoku.split(/\r?\n/);
 
+	const load_bunpou = [];
+	const load_tango = [];
+	const load_yomi = [];
 	
-	for(let i = 0; i < kisoku_lines.length; i++) {
-		const line = kisoku_lines[i].trim();
-		// 文法（文）
-		if(/^\$文\s*→/.test(line)) {
-			
+	// テキストデータから値を抽出
+	{
+		for(let i = 0; i < kisoku_lines.length; i++) {
+			const line = kisoku_lines[i].trim();
+			if(line.length === 0) {
+				continue;
+			}
+			// 文法（文）
+			// 例 $文 → $日付 $の $天気 $聞く
+			if(/^\$文\s*→/.test(line)) {
+				const data = line.replace(/.*→/, "").trim();
+				tango_list = data.split(/\s+/);
+				load_bunpou.push(tango_list);
+			}
+			// 文法（単語集合）
+			// 例 $日付 → 今日 | 明日 | 明後日
+			else if(/^\$[^ \t]+\s*→/.test(line)) {
+				const name = line.replace(/\s*→.*/, "").trim();
+				const data = line.replace(/.*→/, "").trim();
+				word_list = data.split(/\s*\|\s*/);
+				load_tango.push(
+					{
+						name : name,
+						word_list : word_list
+					}
+				);
+			}
+			// よみ
+			// 例 こんにちは こんにちわ
+			else if(/^[^#]+[ \t]+[^#]+$/.test(line)) {
+				const word = line.replace(/[ \t]+[^#]+/, "").trim();
+				const yomi_text = line.replace(/[^#]+[ \t]+/, "").trim();
+				load_yomi.push(
+					{
+						word : word,
+						yomi : yomi_text
+					}
+				);
+			}
 		}
-		// 文法（単語集合）
-		else if(/^\$[^ \t]+\s*→/.test(line)) {
+	}
 
-		}
-		// 音素
-		else if(/^[^#]*$/.test(line)) {
+	// console.log(load_bunpou);
+	// console.log(load_tango);
+	// console.log(load_yomi);
 
+	// $日付 がついているデータを変数名に変更
+	// VAR_AAA のような感じにする
+	{
+		for(const key in load_bunpou) {
+			const word_list = load_bunpou[key];
+			for(const word_key in word_list) {
+				const word = word_list[word_key].replace(/\$/g, "");
+				const yomi = await parseFormat(word);
+				console.log(yomi);
+			}
 		}
-		console.log(line);
-		
 	}
 
 
+	
 }
 
 main();
 
-/*
-const read = async() => {
-    const foo = await parseFormat("を教えてください");
-    console.log(foo);
-}
-read();
-
-
-*/
