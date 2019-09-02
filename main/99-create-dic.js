370002145fb1e7f8fec83d27bbfc145df2b31152
natade-jp <natade3@gmail.com>
2019-09-03 00:22:21 +0900

    up

--- main/99-create-dic.js
+++ main/99-create-dic.js
@@ -34,9 +34,13 @@ const getYomiData = function(morphs) {
 		const morph = morphs[i];
 		reading += morph.reading
 	}
+	const hiragana = MojiJS.toHiragana(reading).replace(/[？！。、]/g , "");
+	const voca = yomi2voca(hiragana);
 	return {
-		hiragana : MojiJS.toHiragana(reading),
-		romaji : MojiJS.toRomajiFromKatakana(reading).toUpperCase()
+		hiragana : hiragana,
+		voca : voca,
+		romaji : MojiJS.toRomajiFromHiragana(hiragana).toUpperCase(),
+		yomi : voca.replace(/ /g , "").toUpperCase()
 	};
 }
 
@@ -63,21 +67,23 @@ const main = async() => {
 	const load_tango = [];
 	const load_yomi = [];
 	
-	// テキストデータから値を抽出
+	// 文法規則とYOMIが記載されたテキストから値を抽出
 	{
 		for(let i = 0; i < kisoku_lines.length; i++) {
 			const line = kisoku_lines[i].trim();
 			if(line.length === 0) {
 				continue;
 			}
-			// 文法（文）
+
+			// 文法（文）データ
 			// 例 $文 → $日付 $の $天気 $聞く
 			if(/^\$文\s*→/.test(line)) {
 				const data = line.replace(/.*→/, "").trim();
 				tango_list = data.split(/\s+/);
 				load_bunpou.push(tango_list);
 			}
-			// 文法（単語集合）
+
+			// 文法（単語集合）データ
 			// 例 $日付 → 今日 | 明日 | 明後日
 			else if(/^\$[^ \t]+\s*→/.test(line)) {
 				const name = line.replace(/\s*→.*/, "").trim();
@@ -90,7 +96,8 @@ const main = async() => {
 					}
 				);
 			}
-			// よみ
+
+			// 読みデータ
 			// 例 こんにちは こんにちわ
 			else if(/^[^#]+[ \t]+[^#]+$/.test(line)) {
 				const word = line.replace(/[ \t]+[^#]+/, "").trim();
@@ -105,6 +112,23 @@ const main = async() => {
 		}
 	}
 
+	// 読みデータを整形
+	// 読みデータがある場合は読みデータを優先させる。
+	const yomi_hash = {};
+	{
+		for(const key in load_yomi) {
+			const hash_key = load_yomi[key].word;
+			const hiragana = MojiJS.toHiragana(load_yomi[key].yomi).replace(/[？！。、]/g , "");
+			const voca = yomi2voca(hiragana);
+			yomi_hash[hash_key] = {
+				hiragana : hiragana,
+				voca : voca,
+				romaji : MojiJS.toRomajiFromHiragana(hiragana).toUpperCase(),
+				yomi : voca.replace(/ /g , "").toUpperCase()
+			}
+		}
+	}
+
 	// console.log(load_bunpou);
 	// console.log(load_tango);
 	// console.log(load_yomi);
@@ -112,14 +136,55 @@ const main = async() => {
 	// $日付 がついているデータを変数名に変更
 	// VAR_AAA のような感じにする
 	{
+		// grammarファイル（Julian文法ファイル）を作成する
+		// S : NS_B HELLO NS_E
+		// HELLO : OHAYOU
+		// HELLO : KONNICHIWA
+
+		const grammar = [];
+
+		// オートマトンによる文法を作成
 		for(const key in load_bunpou) {
 			const word_list = load_bunpou[key];
+			let grammar_line = "S: NS_B ";
 			for(const word_key in word_list) {
 				const word = word_list[word_key].replace(/\$/g, "");
-				const yomi = await parseFormat(word);
-				console.log(yomi);
+				let parse_name = null;
+				// すでにデータがあるならハッシュを使う
+				if(!yomi_hash[word]) {
+					yomi_hash[word] = await parseFormat(word);
+				}
+				parse_name = yomi_hash[word];
+				grammar_line += parse_name.yomi + " ";
 			}
+			grammar_line += "NS_E";
+			grammar.push(grammar_line);
 		}
+
+		// ワードを作成
+		for(const key in load_tango) {
+			const tango_list = load_tango[key];
+			const word = tango_list.name.replace(/\$/g, "");
+			let parse_name = null;
+			// すでにデータがあるならハッシュを使う
+			if(!yomi_hash[word]) {
+				yomi_hash[word] = await parseFormat(word);
+			}
+			parse_name = yomi_hash[word];
+			let grammar_line = parse_name.romaji + ":";
+			for(const word_key in tango_list.word_list) {
+				const word = tango_list.word_list[word_key];
+				if(!yomi_hash[word]) {
+					yomi_hash[word] = await parseFormat(word);
+				}
+				const yomi = yomi_hash[word];
+				grammar_line += " " + yomi.yomi;
+			}
+			grammar.push(grammar_line);
+		}
+
+		console.log(grammar.join("\n"));
+
 	}
 
 
